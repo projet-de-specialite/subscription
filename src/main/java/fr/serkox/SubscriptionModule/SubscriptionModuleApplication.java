f3891546e511aac1997d7d8a2e3d3ea0fe6d963f
augustinde <augustin.1201@outlook.fr>
2023-02-08 22:32:17 +0100

    Initial commit

---
+++ src/main/java/fr/serkox/SubscriptionModule/SubscriptionModuleApplication.java
@@ -0,0 +1,13 @@
+package fr.serkox.SubscriptionModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/serkox/SubscriptionModule/SubscriptionModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.serkox.SubscriptionModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
