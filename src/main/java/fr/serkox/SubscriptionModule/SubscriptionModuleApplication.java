6dc6b0af64b1c810c59d986ba455958d50589082
Augustin D <42963508+augustinde@users.noreply.github.com>
2023-04-11 14:13:29 +0200

    #10 Enable cors origin (#11)

--- src/main/java/fr/serkox/SubscriptionModule/SubscriptionModuleApplication.java
+++ src/main/java/fr/serkox/SubscriptionModule/SubscriptionModuleApplication.java
@@ -2,6 +2,9 @@ package fr.serkox.SubscriptionModule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SubscriptionModuleApplication {
@@ -10,4 +13,13 @@ public class SubscriptionModuleApplication {
         SpringApplication.run(SubscriptionModuleApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/subscription").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
